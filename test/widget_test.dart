048925fa9cc5899f7ec29ee31e90ff580c3c22f5
Paveesh Ravikumar <r.paveesh@gmail.com>
2023-04-24 22:13:38 +0530

    sample unit test added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:track_my_spends/main.dart';
+import 'package:track_my_spends/pages/home_page.dart';
 
 void main() {
-  
+  test('Spends should not be empty', () {
+    final homepage = HomePage();
+
+    int numberOfSpends = homepage.spends.length;
+
+    expect(numberOfSpends, homepage.spends.length);
+  });
 }
 
